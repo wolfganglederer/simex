5dd6ee9f1a26cc0bac7dddb9d1dabb3cce5645aa
Wolfgang Lederer <wolfgang.lederer@gmail.com>
2016-05-14 16:58:11 +0200

    Fixed typo

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -3,10 +3,11 @@ extract.covmat <- function(model) {
   type <- class(model)[1]
   sum.model <- summary(model)
   switch(type , glm = covmat <- sum.model$cov.scaled
-              , lm = covmat <- sum.model$cov.unscaled * sum.model$sigma^2
+              , lm  = covmat <- sum.model$cov.unscaled * sum.model$sigma^2
               , gam = covmat <- model$Vp, nls = covmat <- sum.model$cov.unscaled * sum.model$sigma^2
               , polr= covmat <- vcov(model),
-              , lme = covmat <- model$apVar, nlme = covmat <- model$apVar)
+              , lme = covmat <- model$apVar,
+                nlme = covmat <- model$apVar)
   return(covmat)
 }
 
